c82374b1b50d590bebfeca81f40eabee7d7c9ced
tzafff <christzaf99@gmail.com>
2025-01-18 13:54:34 +0200

    Backend Initial Commit

---
+++ backend/src/test/java/com/ctzaf/jwt/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctzaf.jwt.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
