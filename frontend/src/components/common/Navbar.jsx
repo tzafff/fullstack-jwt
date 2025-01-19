167ed3cc837405feffb57408952a16eb980a0995
tzafff <christzaf99@gmail.com>
2025-01-19 16:03:08 +0200

    Remove import

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {Link, useNavigate} from "react-router-dom";
 import UsersService from "../service/UsersService";
-import usersService from "../service/UsersService";
 
 function Navbar() {
 
