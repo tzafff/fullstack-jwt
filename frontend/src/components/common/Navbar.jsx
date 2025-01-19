4646f4d9615da2b3532c78c9fa0f05e6319b9f5c
tzafff <christzaf99@gmail.com>
2025-01-19 15:22:59 +0200

    NavBar, Footer, Login, Registration Pages.

---
+++ frontend/src/components/common/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import UsersService from "../service/UsersService";
+import usersService from "../service/UsersService";
+
+function Navbar() {
+
+    const isAuthenticated = UsersService.isAuthenticated();
+    const isAdmin = UsersService.isAdmin();
+
+    const handleLogout = () => {
+        const confirmDelete = window.confirm("Are you sure you want to logout?");
+        if(confirmDelete) {
+            UsersService.logout();
+        }
+    };
+
+    return (
+        <nav>
+            <ul>
+                {!isAuthenticated && <li><Link to="/">Phegon Dev</Link></li>}
+                {isAuthenticated && <li><Link to="/profile">Profile</Link></li>}
+                {isAdmin && <li><Link to="/admin/user-management">User Management</Link></li>}
+                {isAuthenticated && <li><Link to="/" onClick={handleLogout}>Logout</Link></li>}
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
